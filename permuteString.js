b8766339b45d5f7638055946cdae6dfdc329ac08
Drew Shoaf <48575629+drewlshoaf@users.noreply.github.com>
2019-04-25 13:30:55 -0400

    Update permuteString.js

--- permuteString.js
+++ permuteString.js
@@ -3,14 +3,14 @@ let minlength = 2;
 
 function permut(term) {
   if (string.length < minlength) return string;
-  var permutations = [];
+  let permutations = [];
   for (var i=0; i<string.length; i++) {
       var char = string[i];
       //no duplicates
       if (string.indexOf(char) != i)
       continue;
       var remainingString = string.slice(0,i) + string.slice(i+1,string.length); 
-      for (var subPermutation of permut(remainingString))
+      for (let subPermutation of permut(remainingString))
           permutations.push(char + subPermutation);
   }
   return permutations;
